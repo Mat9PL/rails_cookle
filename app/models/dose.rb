17ee400aeb0c7d1cb2fcc71ca0764026e575c5ea
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-06-01 11:57:46 +0200

    db schema fixed: doses added to handle n:n relation between ingredients and recipes

---
+++ app/models/dose.rb
@@ -0,0 +1,4 @@
+class Dose < ApplicationRecord
+  belongs_to :recipe
+  belongs_to :ingredient
+end
---
+++ db/migrate/20200601095525_create_doses.rb
@@ -0,0 +1,10 @@
+class CreateDoses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :doses do |t|
+      t.references :recipe, null: false, foreign_key: true
+      t.references :ingredient, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

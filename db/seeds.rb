ba14f00bc76a36d2d22a0cea9a61d2ed48d9a0a1
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-06-01 15:45:58 +0200

    seed fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,25 +11,32 @@ Ingredient.destroy_all
 
 ingredients = %w(eggplant potato egg pasta onion)
 
-ingredients.each { |ingredient| Ingredient.new(name: ingredient) }
+ingredients.each do |ingredient|
+  ingredient = Ingredient.new(name: ingredient)
+  ingredient.save!
+end
 
 20.times do
   name = Faker::Food.dish
   description = Faker::Food.description
   rating = rand(1..5)
 
-  all_ingredients = Ingredient.all
-  added_ingredient = all_ingredients.sample
   
   recipe = Recipe.new(
     name: name,
     description: description,
     rating: rating
     )
-  recipe.save!
-
-  rand(2..5).times do
-    Dose.new(ingredient: added_ingredient, recipe: recipe)
+    recipe.save!
+    
+    all_ingredients = Ingredient.all
+    rand(2..5).times do
+      added_ingredient = all_ingredients.sample
+    p Ingredient.all
+    p added_ingredient
+    p recipe
+    dose = Dose.new(ingredient: added_ingredient, recipe: recipe)
       all_ingredients -= [added_ingredient]
+    dose.save!
   end
 end

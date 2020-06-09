be30982c86b0e7b89af6b630de07a5ec6a2042f5
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-06-09 18:49:59 +0200

    regex builder almost done

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -8,7 +8,7 @@ class Ingredient < ApplicationRecord
 
   def self.create_new(attributes = {})
     name = attributes[:name]
-    new_ingredient = Ingredient.new(name: name, regex: "(\\b#{name}\\b)|(\\b#{name.pluralize}\\b)")
+    new_ingredient = Ingredient.new(name: attributes[:name], regex: build_regex(attributes))
     new_ingredient[:regex] = attributes[:regex] if attributes[:regex]
     new_ingredient
   end
@@ -26,4 +26,16 @@ class Ingredient < ApplicationRecord
     ing_group.save!
     IngredientGrouper.new(ingredient_group: ing_group, ingredient: self).save!
   end
+
+  private
+
+  def build_regex(attributes = {})
+  # example: attributes = {name: 'cheese', forbidden_prefixes: ["gruyère", "stilton", "mascarpone"], synonyms: ["queso"]}
+  forbidden_prefixes = ""
+  name = attributes[:name]
+  attributes[:forbidden_prefixes].each { |prefix| forbidden_prefixes += "(?<!#{prefix + " "})" }
+  regex = "(#{forbidden_prefixes}\\b#{name}\\b)|(#{forbidden_prefixes}\\b#{name.pluralize}\\b)"
+  attributes[:synonyms].each { |synonym| regex += "|(#{forbidden_prefixes}\\b#{synonym}\\b)|(#{forbidden_prefixes}\\b#{synonym.pluralize}\\b)" }
+  regex
+end
 end

5622140246660ec6cb2e1f9db8146c61d08fac6e
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-06-10 18:09:58 +0200

    front improved in index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,26 +5,28 @@ class RecipesController < ApplicationController
   def index
     @ingredient_groups = IngredientGroup.all
     if @search = params[:search].nil?
-      @search = params[:multiple_search][:multiple_search]
+      @search = params[:multiple_search][:multiple_search] - [""]
     else
-      @search = params[:search][:multiple_search]
+      @search = params[:search][:multiple_search] - [""]
     end
-    @searched_ingredients = []
+    @searched_ingredient_ids = []
     @search.each do |ingredient_group_id|
-      unless ingredient_group_id == ""
-        ingredient_group = IngredientGroup.find(ingredient_group_id.to_i)
-        @searched_ingredients += ingredient_group.ingredient_ids
-      end
+      ingredient_group = IngredientGroup.find(ingredient_group_id.to_i)
+      @searched_ingredient_ids += ingredient_group.ingredient_ids
     end
+    @searched_ingredient_groups = @search.map do |id|
+      IngredientGroup.find(id).name
+    end
+    @searched_ingredient_groups = @searched_ingredient_groups
     @all_recipes = policy_scope(Recipe)
-    @found_recipes = select_recipes_including(@searched_ingredients)
+    @found_recipes = select_recipes_including(@searched_ingredient_ids)
     @recipes = @found_recipes.paginate(page: params[:page], per_page: 12)
   end
   
   private
   
-  def select_recipes_including(searched_ingredients)
-    searched_ingredients = searched_ingredients.uniq
-    @all_recipes.select { |recipe| (recipe.ingredient_ids - searched_ingredients.flatten).empty? }
+  def select_recipes_including(searched_ingredient_ids)
+    searched_ingredient_ids = searched_ingredient_ids.uniq
+    @all_recipes.select { |recipe| (recipe.ingredient_ids - searched_ingredient_ids.flatten).empty? }
   end
 end

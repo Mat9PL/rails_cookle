08a20c77b20fc75a9b4bd666c5e7320dd048179f
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-06-12 10:17:44 +0200

    remote: true works correctly, needs ajaxifying

--- app/controllers/cookbook_inclusions_controller.rb
+++ app/controllers/cookbook_inclusions_controller.rb
@@ -4,14 +4,15 @@ class CookbookInclusionsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @cookbook_inclusion = CookbookInclusion.new(cookbook: @cookbook, recipe: @recipe)
     @cookbook_inclusion.save
-    authorize @cookbook_inclusion
-    redirect_to cookbook_path(@cookbook)
-  end
+    # authorize @cookbook_inclusion
+    skip_authorization
+    end
 
   def destroy
     @cookbook_inclusion = CookbookInclusion.find(params[:id])
+    @cookbook = @cookbook_inclusion.cookbook
     @cookbook_inclusion.destroy
-    authorize @cookbook_inclusion
-    redirect_to cookbook_path(@cookbook)
-  end
+    # authorize @cookbook_inclusion
+    skip_authorization
+    end
 end
--- app/controllers/dispenses_controller.rb
+++ app/controllers/dispenses_controller.rb
@@ -1,6 +1,6 @@
 class DispensesController < ApplicationController
   def show
-    @ingredient_groups = IngredientGroup.all.reverse
+    @ingredient_groups = IngredientGroup.all
     if Dispense.where(user: current_user).empty?
       @dispense = Dispense.new(user: current_user)
       @dispense.save
@@ -8,7 +8,6 @@ class DispensesController < ApplicationController
       @dispense = Dispense.where(user: current_user).first
     end
     @ingredient_group_ids = @dispense.ingredient_groups.map {|ing| ing.id.to_s }
-    authorize @dispense
-
+    skip_authorization
   end
 end

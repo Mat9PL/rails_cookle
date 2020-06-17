911182121b4e645399016d7fa862aca05efa021c
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-06-18 01:28:33 +0200

    pantry inclusions and pantry refactored

--- app/controllers/pantry_inclusions_controller.rb
+++ app/controllers/pantry_inclusions_controller.rb
@@ -1,15 +1,29 @@
 class PantryInclusionsController < ApplicationController
   def create
-    @pantry = Pantry.find(params[:pantry_id])
-    @input = params[:search][:multiple_search] - [""]
-    @searched_ingredients = @input.map do |ingredient_group_id|
-      IngredientGroup.find(ingredient_group_id.to_i)
-    end
-    @pantry.Pantry_inclusions.destroy_all
-    @searched_ingredients.each do |ing|
-      PantryInclusion.new(Pantry: @pantry, ingredient_group: ing).save
-    end
+    set_pantry
+    set_input # i.e. get the ingredient groups typed by the user from params
+    set_input_ingredients
+    update_pantry_inclusions # destroy previous and add new
     authorize @pantry
-    redirect_to Pantry_path(@pantry)
+    redirect_to pantry_path(@pantry)
+  end
+  
+  private
+  
+  def set_pantry
+    @pantry = Pantry.find(params[:pantry_id])
+  end
+  
+  def set_input
+    @input = params[:search][:multiple_search] - [""]
+  end
+  
+  def set_input_ingredients
+    @input_ingredients = @input.map { |id| IngredientGroup.find(id.to_i) }
+  end
+  
+  def update_pantry_inclusions
+    @pantry.pantry_inclusions.destroy_all
+    @input_ingredients.each { |ing| PantryInclusion.create(pantry: @pantry, ingredient_group: ing) }
   end
 end

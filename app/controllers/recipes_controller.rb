b3edd19671218c981d772cb4c7804745cce38e02
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-06-18 00:07:45 +0200

    code simplified and documented

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,23 +3,45 @@ require 'will_paginate/array'
 class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
+    policy_scope(Recipe)
+    set_cookbook # for the link to the cookbook in the navbar
+    set_ingredient_groups # for the select2 in the search bar
+    get_search_results
+    set_searched_ingredient_groups # to pre-populate the search bar
+    set_found_recipes # to get the number of all the recipes found
+    set_recipes # to get paginated list of recipes to display
+  end
+
+  private
+
+  def set_recipes
+    @recipes = @found_recipes.paginate(page: params[:page], per_page: 10)
+  end
+
+  def set_searched_ingredient_groups
+    @searched_ingredient_groups = @search.map { |id| IngredientGroup.find(id) }
+  end
+
+  def set_found_recipes
+    ingredient_ids = convert_group_ids_to_ingredient_ids(@search)
+    @found_recipes = select_recipes_including_only(ingredient_ids)
+  end
+
+  def set_cookbook
+    Cookbook.where(user: current_user).empty? ? @cookbook = Cookbook.create(user: current_user) : @cookbook = current_user.cookbook  
+  end
+
+  def set_ingredient_groups
     @ingredient_groups = IngredientGroup.all
-    
+  end
+
+  def get_search_results
     if params[:search].nil?
       @search = params[:multiple_search][:multiple_search] - [""]
     else
       @search = params[:search][:multiple_search] - [""]
     end
-    
-    @searched_ingredient_groups = @search.map { |id| IngredientGroup.find(id) }
-    @all_recipes = policy_scope(Recipe)
-    @ingredient_ids = convert_group_ids_to_ingredient_ids(@search)
-    @found_recipes = select_recipes_including_only(@ingredient_ids)
-    @recipes = @found_recipes.paginate(page: params[:page], per_page: 10)
-    Cookbook.where(user: current_user).empty? ? @cookbook = Cookbook.create(user: current_user) : @cookbook = current_user.cookbook    
   end
-
-  private
 
   def convert_group_ids_to_ingredient_ids(array)
     ingredient_ids = []
